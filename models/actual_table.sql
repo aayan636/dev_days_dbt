87402bf0b8622b636fcdaf2c50859db0e9f69f94
Tester Dude (tester@etleap.com) <tester@etleap.com>
2026-01-08 01:14:45 +0000

    Updated actual_table.

--- models/actual_table.sql
+++ models/actual_table.sql
@@ -3,14 +3,13 @@ WITH t1 AS (
     FROM {{ source('aayan_test', 'Aayan_S3_Lots_Of_Small_Files') }}
 ),
 t2 AS (
-    SELECT row_number() OVER () AS rn, *
+    SELECT row_number() OVER () AS rm, *
     FROM {{ ref('another_table') }}
 )
 SELECT
-    t1.rn,
     t1.*,
     t2.*
 FROM t1
 FULL OUTER JOIN t2
-    ON t1.rn = t2.rn
-ORDER BY COALESCE(t1.rn, t2.rn)
+    ON t1.rn = t2.rm
+ORDER BY COALESCE(t1.rn, t2.rm)
