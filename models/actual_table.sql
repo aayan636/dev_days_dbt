660b62759ba3eba653bd2a9816727c210a6a232a
Tester Dude (tester@etleap.com) <tester@etleap.com>
2026-01-08 01:13:11 +0000

    Added another_table. Updated actual_table.

--- models/actual_table.sql
+++ models/actual_table.sql
@@ -1,1 +1,16 @@
-SELECT * FROM {{ source('aayan_test', 'Aayan_S3_Lots_Of_Small_Files') }}
+WITH t1 AS (
+    SELECT row_number() OVER () AS rn, *
+    FROM {{ source('aayan_test', 'Aayan_S3_Lots_Of_Small_Files') }}
+),
+t2 AS (
+    SELECT row_number() OVER () AS rn, *
+    FROM {{ ref('another_table') }}
+)
+SELECT
+    t1.rn,
+    t1.*,
+    t2.*
+FROM t1
+FULL OUTER JOIN t2
+    ON t1.rn = t2.rn
+ORDER BY COALESCE(t1.rn, t2.rn)
