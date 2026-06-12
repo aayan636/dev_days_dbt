fdff9e2d6b04318505e2c96a2af059ba90e827da
Tester Dude (tester@etleap.com) <tester@etleap.com>
2026-06-12 19:03:31 +0000

    Updated model_1.

--- models/model_1.sql
+++ models/model_1.sql
@@ -1,6 +1,5 @@
-{% set dependency0 = {{ source('aayan_test', 'file_xml9') }}  %}
-{% set dependency1 = {{ source('aayan_test', 'file_xml2') }}  %}
-{% set dependency2 = {{ source('aayan_test', 'file_xml3') }}  %}
+{% set dependency0 = source('aayan_test', 'file_xml9') %}
+{% set dependency1 = source('aayan_test', 'file_xml2') %}
+{% set dependency2 = source('aayan_test', 'file_xml3') %}
 
--- SELECT *, '{{ env_var("DBT_KEY") }}' as dbt_key FROM {{ dependency0 }}
-select * {{ dependency0 }}
+select * from {{ dependency0 }}
